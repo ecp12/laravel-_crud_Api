a849417af7d8326c429ee5c2a96e1b27994a6438
ecp12 <106453089+ecp12@users.noreply.github.com>
2023-02-04 21:31:24 +0100

    new crud image

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\PorfolioController;
+use App\Http\Controllers\Api\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('porfolios', PorfolioController::class );
+Route::post('image',[ImageController::class, 'imageStore']);
