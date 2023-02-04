c2a2ecf241895ef351491195aa1efa61be321fb7
ecp12 <106453089+ecp12@users.noreply.github.com>
2023-02-05 00:34:29 +0100

    new post api images

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\PorfolioController;
 use App\Http\Controllers\APIImageController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('porfolios', PorfolioController::class );
 Route::post('images', [APIImageController::class, 'upload']);
+Route::apiResource('posts', PostController::class);
